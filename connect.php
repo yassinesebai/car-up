5765f1af69091621091e04c680273251d1135c5f
yassinssebai <yassinessebai@gmail.com>
2022-09-06 04:07:09 +0100

    final

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 
 // Create connection
 global $conn;
-$conn = new mysqli("localhost", "root", "", "location");
+$conn = new mysqli("eu-cdbr-west-03.cleardb.net", "bff448eb837429", "056af6fd", "heroku_41ea1ab9f8aba65");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
